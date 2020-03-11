3a3a1cdbec00dfacf7f7c3148d2f044287794d5c
George Alonge <georgedelson@gmail.com>
2020-03-11 23:09:33 +0100

    update google and android play links to point to form

--- src/components/store.component.tsx
+++ src/components/store.component.tsx
@@ -12,10 +12,10 @@ const StoreSection = () => {
                 <h3 className="manifest_text"><span>Find time for</span> <br/>important things.</h3>
                 <ul className="nav manifest_downloadBtns">
                     <li className="nav-item">
-                        <a className="nav-link" href={createEmptyLink()}><img src="svgs/download-on-the-app-store-apple.svg" alt="download-on-the-app-store-apple" /></a>
+                        <a className="nav-link" href="https://forms.gle/E7twpo5fJ3abxyrR9" target="_blank" ><img src="svgs/download-on-the-app-store-apple.svg" alt="download-on-the-app-store-apple" /></a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href={createEmptyLink()}><img src="svgs/download-on-the-google-store.svg" alt="download-on-the-app-store-apple" /></a>
+                        <a className="nav-link" href="https://forms.gle/E7twpo5fJ3abxyrR9" target="_blank" ><img src="svgs/download-on-the-google-store.svg" alt="download-on-the-app-store-apple" /></a>
                     </li>
                 </ul>
             </div>
