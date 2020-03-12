f0a3b248f531feaafb80e97d6e5457b85e457ad3
bolaji <bioyeneye@gmail.com>
2020-03-12 12:39:40 +0100

    added contact us

--- src/components/footer.component.tsx
+++ src/components/footer.component.tsx
@@ -5,62 +5,62 @@ import { createMailtoLink, createEmptyLink } from '../shared/helpers.ts/linkhelp
 
 const FooterSection = () => {
     return (
-        <footer className="footer">
-        <div className="footer-content row align-items-baseline">
-            <div className="col-12 col-xl-3 brandLogo">
-                <a className="navbar-brand" href="https://www.liferithims.com">
-                    <img src="svgs/lifechitect_full_logo_white.svg" alt="" />
-                </a>
-            </div>
-            <div className="col-12 col-xl-8 footer_items">
-                <div className="address">
-                    <address>
-                        Liferithms, Inc.<br />
+        <footer className="footer" id="contactus">
+            <div className="footer-content row align-items-baseline">
+                <div className="col-12 col-xl-3 brandLogo">
+                    <a className="navbar-brand" href="https://www.liferithims.com">
+                        <img src="svgs/lifechitect_full_logo_white.svg" alt="" />
+                    </a>
+                </div>
+                <div className="col-12 col-xl-8 footer_items">
+                    <div className="address">
+                        <address>
+                            Liferithms, Inc.<br />
                         995 Market St,<br />
                         San Francisco, CA 94103<br />
-                        <a href={createMailtoLink()}>info@liferithms.com</a>
-                    </address>
-                </div>
-                <div className="footer_links">
-                    <ul className="nav flex-column">
-                        <li className="nav-item">
-                            <a className="nav-link" href={createEmptyLink()} target="_blank" rel="noopener noreferrer">Privacy Policy</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href={createEmptyLink()} target="_blank" rel="noopener noreferrer">Terms of Use</a>
-                        </li>
-                    </ul>
-                </div>
-                <div className="socials">
-                    <p>Follow us</p>
-                    <ul className="nav">
-                        <li className="nav-item">
-                            <a className="nav-link" href="https://instagram.com/lifechitectapp" target="_blank" rel="noopener noreferrer">
-                                <img src="svgs/instagram.svg" alt="Instagram" />
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="https://twitter.com/lifechitectapp" target="_blank" rel="noopener noreferrer">
-                                <img src="svgs/twitter.svg" alt="Twitter" />
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="https://linkedin.com/company/liferithms" target="_blank" rel="noopener noreferrer">
-                                <img src="svgs/linkedIn.svg" alt="LinkedIn" />
-                            </a>
-                        </li>
-                        {/* <li className="nav-item">
+                            <a href={createMailtoLink()}>info@liferithms.com</a>
+                        </address>
+                    </div>
+                    <div className="footer_links">
+                        <ul className="nav flex-column">
+                            <li className="nav-item">
+                                <a className="nav-link" href={createEmptyLink()} target="_blank" rel="noopener noreferrer">Privacy Policy</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href={createEmptyLink()} target="_blank" rel="noopener noreferrer">Terms of Use</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="socials">
+                        <p>Follow us</p>
+                        <ul className="nav">
+                            <li className="nav-item">
+                                <a className="nav-link" href="https://instagram.com/lifechitectapp" target="_blank" rel="noopener noreferrer">
+                                    <img src="svgs/instagram.svg" alt="Instagram" />
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="https://twitter.com/lifechitectapp" target="_blank" rel="noopener noreferrer">
+                                    <img src="svgs/twitter.svg" alt="Twitter" />
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="https://linkedin.com/company/liferithms" target="_blank" rel="noopener noreferrer">
+                                    <img src="svgs/linkedIn.svg" alt="LinkedIn" />
+                                </a>
+                            </li>
+                            {/* <li className="nav-item">
                             <a className="nav-link" href={createEmptyLink()} target="_blank" rel="noopener noreferrer">
                                 <img src="svgs/medium.svg" alt="Medium" />
                             </a>
                         </li> */}
-                    </ul>
-                    <p className="copyright">© 2020 Liferithms. All rights reserved.</p>
+                        </ul>
+                        <p className="copyright">© 2020 Liferithms. All rights reserved.</p>
+                    </div>
                 </div>
+
             </div>
-            
-        </div>
-    </footer>
+        </footer>
     );
 }
 
--- src/components/nav.component.tsx
+++ src/components/nav.component.tsx
@@ -38,7 +38,7 @@ export default class Nav extends Component<{}, NavState> {
                         <a className="nav-link" href="#howItWorks">How It Works</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#works">Contact Us</a>
+                        <a className="nav-link" href="#contactus">Contact Us</a>
                     </li>
                 </ul>
                 <button className="navbar-toggler d-md-none" onClick={this.toggleMenu}>
@@ -52,7 +52,7 @@ export default class Nav extends Component<{}, NavState> {
                             <a className="nav-link" href="#howItWorks">How It Works</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#works">Contact Us</a>
+                            <a className="nav-link" href="#contactus">Contact Us</a>
                         </li>
                     </ul>
                 </div>
